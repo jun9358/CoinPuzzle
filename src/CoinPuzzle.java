0a6d4fc564ee2b42d85efed672c847c1ed237bec
Jun-ho, Song <jun9358@gmail.com>
2013-05-26 01:49:03 +0900

    Add main frame
    Add main frame that show in first.
    It will contain board panel, status label and two buttons.

---
+++ src/CoinPuzzle.java
@@ -0,0 +1,13 @@
+import java.awt.Frame;
+
+
+public class CoinPuzzle
+{	
+	public static void main(String []args)
+	{
+		Frame fraMain = new FrameCoinPuzzle();
+		
+		fraMain.setTitle("Coin-sliding Puzzle");
+		fraMain.setVisible(true);
+	}
+}
---
+++ src/FrameCoinPuzzle.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+
+public class FrameCoinPuzzle extends JFrame
+{
+	private static final int FRAME_WIDTH = 300;
+	private static final int FRAME_HEIGHT = 400;
+	
+	FrameCoinPuzzle()
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(FRAME_WIDTH, FRAME_HEIGHT);
+	}
+}
