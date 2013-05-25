36bf65e520e79478bc4483cee52fc5d12bc6b77b
Jun-ho, Song <jun9358@gmail.com>
2013-05-26 03:34:21 +0900

    Add components and modify some others
    Add components such as panel to play, label and buttons in
    FrameCoinPuzzle.
    To alignment, I changed overal size to inner size.
    To get border's size, put setVisible() method in head.

--- src/FrameCoinPuzzle.java
+++ src/FrameCoinPuzzle.java
@@ -1,14 +1,73 @@
+import java.awt.Dimension;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 
 public class FrameCoinPuzzle extends JFrame
 {
-	private static final int FRAME_WIDTH = 300;
-	private static final int FRAME_HEIGHT = 400;
+	private static final int FRAME_INNER_WIDTH = 300;
+	private static final int FRAME_INNER_HEIGHT = 400;
+	private static final int FRAME_MARGIN = 50;
+	
+	private JPanel pnlBoard;
+	private JLabel lblStatus;
+	private JButton btnReset;
+	private JButton btnExit;
+	private static final int BUTTON_WIDTH = 75;
+	private static final int BUTTON_HEIGHT = 25;
 	
 	FrameCoinPuzzle()
 	{
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		// This code can be using getInsets() method to get border size
+		setVisible(true);
+		setVisible(false);
+		
+		// Set properties
+		setSize
+		(
+			FRAME_INNER_WIDTH + getInsets().left + getInsets().right,
+			FRAME_INNER_HEIGHT + getInsets().top + getInsets().bottom
+		);
+		setLayout(null);
+		
+		// Create components
+		pnlBoard = new PanelBoard();
+		pnlBoard.setBounds
+		(
+			0, 0,
+			pnlBoard.getPreferredSize().width, pnlBoard.getPreferredSize().height
+		);
+		
+		lblStatus = new JLabel("Transform the H to an O");
+		lblStatus.setBounds
+		(
+			0, pnlBoard.getSize().height,
+			FRAME_INNER_WIDTH, 50
+		);
+		lblStatus.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		btnReset = new JButton("Reset");
+		btnReset.setBounds
+		(
+			FRAME_MARGIN, lblStatus.getBounds().y + lblStatus.getBounds().height,
+			BUTTON_WIDTH, BUTTON_HEIGHT
+		);
+		
+		btnExit = new JButton("Exit");
+		btnExit.setBounds
+		(
+			FRAME_INNER_WIDTH - (FRAME_MARGIN + BUTTON_WIDTH), lblStatus.getBounds().y + lblStatus.getBounds().height,
+			BUTTON_WIDTH, BUTTON_HEIGHT
+		);
+		
+		// Add components
+		this.add(pnlBoard);
+		this.add(lblStatus);
+		this.add(btnReset);
+		this.add(btnExit);
 	}
 }
