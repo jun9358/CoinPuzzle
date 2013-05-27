0ebc4f0487146b5ea934661940474fae4300fb6e
Jun-ho, Song <jun9358@gmail.com>
2013-05-28 00:41:44 +0900

    Add moving coin component
    Add moving coin component.
    But, I don't know putting CoinComponent.java is right.

--- src/CoinComponent.java
+++ src/CoinComponent.java
@@ -1,21 +1,35 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.font.TextAttribute;
 import java.text.AttributedString;
 
 import javax.swing.JComponent;
 
 
-public class CoinComponent extends JComponent
+public class CoinComponent extends JComponent implements MouseMotionListener, MouseListener
 {
 	private int coinNumber;
 	private static final int COIN_WIDTH = 30;
 	private static final int COIN_HEIGHT = 30;
+	private static Point clkPoint;
 	
 	CoinComponent(int _coinNumber)
 	{
 		coinNumber = _coinNumber;
+		
+		// Add mouse listener of coin
+		this.addMouseMotionListener(this);
+		this.addMouseListener(this);
+	}
+	
+	public void setClickedPoint(Point _clkPoint)
+	{
+		clkPoint = _clkPoint;
 	}
 	
 	public void paintComponent(Graphics g)
@@ -30,5 +44,55 @@ public class CoinComponent extends JComponent
 		attrString.addAttribute(TextAttribute.FOREGROUND, Color.white);
 		
 		g2.drawString(attrString.getIterator(), 10, 23);
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e)
+	{
+		this.setLocation(e.getLocationOnScreen().x - this.getRootPane().getX() - (int)clkPoint.getX(),
+						 e.getLocationOnScreen().y - this.getRootPane().getY() - (int)clkPoint.getY());
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e)
+	{
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e)
+	{
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e)
+	{
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e)
+	{
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e)
+	{
+		if (clkPoint == null)
+		{
+			clkPoint = new Point(e.getX(), e.getY());
+		}
+		else
+		{
+			clkPoint.setLocation(e.getX(), e.getY());
+		}
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e)
+	{
+		
 	}
 }
