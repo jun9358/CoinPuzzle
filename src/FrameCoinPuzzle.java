d95c74c3c3779082dc413d61c64e477922257991
Jun-ho, Song <jun9358@gmail.com>
2013-05-29 14:22:10 +0900

    Add actionlistener of button and fix bug
    Add ActionListener of btnReset and btnExit.
    If btnReset was clicked, the board would be initlized.
    If btnExit was clicked, this program would be quit.
    
    The member variable 'pnlBoard' should be PanelBoard, so I fix it.

--- src/FrameCoinPuzzle.java
+++ src/FrameCoinPuzzle.java
@@ -1,19 +1,19 @@
-import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 
-public class FrameCoinPuzzle extends JFrame
+public class FrameCoinPuzzle extends JFrame implements ActionListener
 {
 	private static final int FRAME_INNER_WIDTH = 300;
 	private static final int FRAME_INNER_HEIGHT = 400;
 	private static final int FRAME_MARGIN = 50;
 	
-	private JPanel pnlBoard;
+	private PanelBoard pnlBoard;
 	private JLabel lblStatus;
 	private JButton btnReset;
 	private JButton btnExit;
@@ -57,6 +57,7 @@ public class FrameCoinPuzzle extends JFrame
 			FRAME_MARGIN, lblStatus.getBounds().y + lblStatus.getBounds().height,
 			BUTTON_WIDTH, BUTTON_HEIGHT
 		);
+		btnReset.addActionListener(this);
 		
 		btnExit = new JButton("Exit");
 		btnExit.setBounds
@@ -64,11 +65,29 @@ public class FrameCoinPuzzle extends JFrame
 			FRAME_INNER_WIDTH - (FRAME_MARGIN + BUTTON_WIDTH), lblStatus.getBounds().y + lblStatus.getBounds().height,
 			BUTTON_WIDTH, BUTTON_HEIGHT
 		);
+		btnExit.addActionListener(this);
 		
 		// Add components
 		this.add(pnlBoard);
 		this.add(lblStatus);
 		this.add(btnReset);
 		this.add(btnExit);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		// If source was extended by CoinComponent.
+		if (JButton.class.equals(e.getSource().getClass()))
+		{
+			if (e.getSource() == btnReset)
+			{
+				pnlBoard.initBoard();
+			}
+			else if (e.getSource() == btnExit)
+			{
+				System.exit(0);
+			}			
+		}
 	}
 }
